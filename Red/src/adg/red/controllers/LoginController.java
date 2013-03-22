094b5591bc14000158b81f29c6fe38293886cbd5
Witty <wittchot@mss.icics.ubc.ca>
2013-03-22 01:43:02 -0700

    add action to login button to load a student home screen when pressed.

--- Red/src/adg/red/controllers/LoginController.java
+++ Red/src/adg/red/controllers/LoginController.java
@@ -10,8 +10,15 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import adg.red.BootStrap;
 
 /**
  *
@@ -21,16 +28,40 @@ public class LoginController implements Initializable {
     
     @FXML //  fx:id="close"
     private MenuItem close; // Value injected by FXMLLoader
+    @FXML //  fx:id="loginBtn"
+    private Button loginBtn; // Value injected by FXMLLoader
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        // setOnAction when close menuitem is selected
         close.setOnAction(new EventHandler<ActionEvent>() {
-
-          @Override
-          public void handle(ActionEvent event) {
-            Platform.exit();
-          }
+            
+            @Override
+            public void handle(ActionEvent event) {
+                Platform.exit();
+            }
+        });
+        
+        // setOnAction when login button is pressed
+        loginBtn.setOnAction(new EventHandler<ActionEvent>()  {
+                        
+            @Override
+            public void handle(ActionEvent event) {
+                Parent root = null;
+                try {
+                    BootStrap boot = new BootStrap();
+                    root = FXMLLoader.load(getClass().getResource(boot.getUserInterfaceUrl("HomeStudent")));
+                }
+                catch (Exception ex){
+                    //handle error??
+                }
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
         });
     }    
 }
