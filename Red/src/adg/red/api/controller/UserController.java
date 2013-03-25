ad757d013ba1aaa9884221f421f7617c9afe9ff1
nicole <nicoleyu2003@gmail.com>
2013-03-25 10:59:59 -0700

    controllers and models added

---
+++ Red/src/adg/red/api/controller/UserController.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adg.red.api.controller;
+
+
+import adg.red.api.model.User;
+import adg.red.api.util.Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jingboyu
+ */
+public class UserController {
+ /*   
+    User usr;
+    public UserController(String uid){
+        
+        usr = new User(uid);
+    }
+    
+    public String showProfile(){
+        
+        
+        return usr.executeShowProfileSql();
+        
+    }
+    
+    public boolean changePassword(String uid, String oldpwd, String newpwd){
+        
+        //valid old password
+        //set new password
+        //inform user the update succeed or failed
+        usr.validateOldPwd(oldpwd);
+       return usr.setNewPwd(newpwd);
+       
+    }
+    
+    public String editFirstName(String fname){
+        
+        //set new name
+        //return updated name & display it to user
+        return usr.setFirstName(fname);
+    }
+    
+     public String editLastName(String lname){
+        
+         //set new name
+         //return updated name & display it to user
+        return usr.setLastName(lname);
+    }
+     
+     public Integer editPhoneNumber(Integer phoneNumber){
+        
+        //set new phone number
+        //return updated phone number & display it to user
+        return usr.setPhoneNumber(phoneNumber);
+    }
+     
+    public String editDateOfBirth(String dateofbirth){
+        
+         //set new DOB
+        //return updated DOB & display it to user
+        return usr.setDateOfBirth(dateofbirth);
+    }
+    
+    public String editEmail(Integer email){
+        
+        //set new email
+        //return updated email  & display it to user
+        return usr.setEmail(email);
+    }
+    
+    public String editAddress(Integer addr){
+        
+        //set new address
+        //return updated adress  & display it to user
+        return usr.setAddress(addr);
+    } 
+    */
+}
+
