a98c453afac3b9f14da3121b44b87a3fa260b67c
Witty <wittchot@mss.icics.ubc.ca>
2013-03-26 02:46:47 -0700

    add a to c hyperlink action

--- Red/src/adg/red/api/model/Department.java
+++ Red/src/adg/red/api/model/Department.java
@@ -4,10 +4,33 @@
  */
 package adg.red.api.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  *
  * @author jingboyu
  */
 public class Department {
     
+    private StringProperty deptId;
+
+    
+    private StringProperty deptName;
+
+    public Department(String deptId, String deptName) {
+        this.deptId = new SimpleStringProperty(deptId);
+        this.deptName = new SimpleStringProperty(deptName);
+    }
+
+    public StringProperty deptIdProperty() { return deptId; }
+    public StringProperty deptNameProperty() { return deptName; }
+    
+    public String getDeptId() {
+        return deptId.get();
+    }
+
+    public String getDeptName() {
+        return deptName.get();
+    }
 }
