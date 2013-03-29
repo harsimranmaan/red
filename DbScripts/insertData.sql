d258159502b400c4e484b77da986aa09b0f837d6
Harsimran Singh Maan <maan.harry@gmail.com>
2013-03-28 17:16:33 -0700

    Add db scripts

---
+++ DbScripts/insertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO `UserType` VALUES (0,'Student'),(1,'Faculty'),(2,'Admin');
+INSERT INTO `GradingScheme` VALUES (0,'Percentage'),(1,'Pass/Fail');
+INSERT INTO `SectionType` VALUES (0,'Lecture'),(1,'Tutorial'),(2,'Lab');
+
