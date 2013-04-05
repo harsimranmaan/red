3342bf44ee6c0ec093e7fc0aa4d866d87a3d4a2b
Bahman Razmpa <razmpa@gmail.com>
2013-04-05 01:58:03 -0700

    Message subject/date/sender now working

--- Red/src/adg/red/controllers/MessageController.java
+++ Red/src/adg/red/controllers/MessageController.java
@@ -9,9 +9,11 @@ import adg.red.models.Message;
 import adg.red.models.MessageReceiver;
 import adg.red.models.User;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
@@ -29,7 +31,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 public class MessageController implements Initializable
 {
 
-    private TableColumn<Message, String> subjectCol;
     @FXML
     private MenuItem btnDelete;
     @FXML
@@ -50,6 +51,21 @@ public class MessageController implements Initializable
     private TableView<Message> tblMessages;
     private User currentUser;
 
+    @FXML
+    private void deleteMessage(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void markMessageRead(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void markMessageUnread(ActionEvent event)
+    {
+    }
+
     /**
      * Initializes the controller class.
      */
@@ -60,16 +76,20 @@ public class MessageController implements Initializable
         String userId = currentUser.getUsername();
         List<Integer> messageId;
         messageId = MessageReceiver.findMessageByReceiverId(userId);
-        List<Message> messages = null;
-//        for (int i = 0; i < messageId.size(); i++)
-//        {
-//            messages.add(Message.findMessageByMessageId(messageId.get(i)));
-//        }
+        List<Message> messages = new ArrayList<Message>();
+        for (int i = 0; i < messageId.size(); i++)
+        {
+            messages.add(Message.findMessageByMessageId(messageId.get(i)));
+        }
         populateMessageList(messages);
+
     }
 
     public void populateMessageList(List<Message> messages)
     {
         colSubject.setCellValueFactory(new PropertyValueFactory<Message, String>("subject"));
+        colDate.setCellValueFactory(new PropertyValueFactory<Message, Date>("dateTime"));
+        colSender.setCellValueFactory(new PropertyValueFactory<Message, String>("senderId"));
+        tblMessages.getItems().setAll(messages);
     }
 }
