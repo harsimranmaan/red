edf8eb08590be89fab5af3ef3bf3645c063e3bf5
Bahman Razmpa <razmpa@gmail.com>
2013-04-04 23:17:09 -0700

    more work on the messages - not functional yet

--- Red/src/adg/red/controllers/MessageController.java
+++ Red/src/adg/red/controllers/MessageController.java
@@ -4,14 +4,22 @@
  */
 package adg.red.controllers;
 
+import adg.red.models.Course;
+import adg.red.models.Message;
+import adg.red.models.MessageReceiver;
+import adg.red.models.User;
 import java.net.URL;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -21,24 +29,26 @@ import javafx.scene.control.TextArea;
 public class MessageController implements Initializable
 {
 
-    @FXML
-    private TableColumn<?, ?> subjectCol;
-    @FXML
-    private TableColumn<?, ?> dateCol;
-    @FXML
-    private TableColumn<?, ?> senderCol;
-    @FXML
-    private TableColumn<?, ?> statusCol;
-    @FXML
-    private TextArea messageBody;
-    @FXML
-    private SplitMenuButton btnMessageAction;
+    private TableColumn<Message, String> subjectCol;
     @FXML
     private MenuItem btnDelete;
     @FXML
     private MenuItem btnMarkRead;
     @FXML
     private MenuItem btnMarkUnread;
+    @FXML
+    private TableColumn<Message, String> colSubject;
+    @FXML
+    private TableColumn<Message, Date> colDate;
+    @FXML
+    private TableColumn<Message, String> colSender;
+    @FXML
+    private TableColumn<Message, String> colStatus;
+    @FXML
+    private TextArea txtMessageBody;
+    @FXML
+    private TableView<Message> tblMessages;
+    private User currentUser;
 
     /**
      * Initializes the controller class.
@@ -46,6 +56,20 @@ public class MessageController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        currentUser = adg.red.utils.Context.getInstance().getCurrentUser();
+        String userId = currentUser.getUsername();
+        List<Integer> messageId;
+        messageId = MessageReceiver.findMessageByReceiverId(userId);
+        List<Message> messages = null;
+//        for (int i = 0; i < messageId.size(); i++)
+//        {
+//            messages.add(Message.findMessageByMessageId(messageId.get(i)));
+//        }
+        populateMessageList(messages);
+    }
+
+    public void populateMessageList(List<Message> messages)
+    {
+        colSubject.setCellValueFactory(new PropertyValueFactory<Message, String>("subject"));
     }
 }
