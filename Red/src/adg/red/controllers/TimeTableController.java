af8f3ca6ab6b04951c225d5a3e0cde0b0cdb1d2b
Jaspreet Thind <jayzthind@gmail.com>
2013-04-07 02:44:59 -0700

    Make grid dynamic

--- Red/src/adg/red/controllers/TimeTableController.java
+++ Red/src/adg/red/controllers/TimeTableController.java
@@ -11,10 +11,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import adg.red.utils.Context;
+import adg.red.utils.DateFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 /**
  * FXML Controller class
@@ -27,6 +35,33 @@ public class TimeTableController implements Initializable
     @FXML
     private GridPane gdpTimeTable;
 
+    private void initGrid(int dayListSize)
+    {
+        int width = 100 / (dayListSize + 1);
+
+        for (int counter = 0; counter <= dayListSize; counter++)
+        {
+            ColumnConstraints constraints = new ColumnConstraints();
+            constraints.setPercentWidth(width);
+            constraints.setHgrow(Priority.ALWAYS);
+            constraints.setHalignment(HPos.CENTER);
+            gdpTimeTable.getColumnConstraints().add(constraints);
+        }
+
+        int numberOfRows = 25;
+        int height = 100 / numberOfRows;
+        for (int row = 0; row < numberOfRows; row++)
+        {
+            RowConstraints constraints = new RowConstraints();
+            constraints.setPercentHeight(height);
+            constraints.setVgrow(Priority.ALWAYS);
+            constraints.setValignment(VPos.CENTER);
+            gdpTimeTable.getRowConstraints().add(constraints);
+        }
+
+        gdpTimeTable.setSnapToPixel(false);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
@@ -36,6 +71,9 @@ public class TimeTableController implements Initializable
         List<SectionTimeTable> timeTableList = Context.getInstance().getTimeTable();
 
         List<WeekDay> dayList = WeekDay.getAllWeekDay();
+        int dayListSize = dayList.size();
+        initGrid(dayListSize);
+
         int col = 1;
         for (WeekDay weekDay : dayList)
         {
@@ -44,27 +82,36 @@ public class TimeTableController implements Initializable
         }
 
         int hrs = 8;
-        for (int rows = 1; rows < 25; hrs++)
+        for (int row = 1; row < 25; hrs++)
         {
             Label label = new Label(hrs + ":00");
-            gdpTimeTable.add(label, 0, rows++);
-            rows++;
-//            Label label1 = new Label(hrs + ":30");
-//            gdp.add(label1, 0, rows++);
+            gdpTimeTable.add(label, 0, row++);
+            row++;
         }
 
+        int cols = 1;
+        int rows = 1;
+        if (timeTableList != null)
+        {
+            for (SectionTimeTable table : timeTableList)
+            {
+                cols = table.getSectionTimeTablePK().getDayId() % 100 + 1;
+                int hour = Integer.parseInt(DateFormatter.formatHour(table.getSectionTimeTablePK().getStartTime()));
+                int mins = Integer.parseInt(DateFormatter.formatMins(table.getSectionTimeTablePK().getStartTime()));
 
-//            if (SectionTimeTable.getBySectionPK() != null){
-//                List<SectionTimeTable> list = SectionTimeTable.getBySectionPK();//enrol.getSection().getSectionPK());
-//                for (SectionTimeTable table : list)
+//                int baseHr = 8;
+//                for (int row = 1; row < 25; baseHr++)
 //                {
-//                    for(int col = 1 ; col < 8; col++)
-//                        for(int row = 1; row <25; row++ )
-//                    Label label = new Label(table.getSectionTimeTablePK().getDepartmentId() + " " + table.getSectionTimeTablePK().getCourseNumber());
-//                    gdp.add(label, 0, i++);
-//                    System.out.println("" + table.getSectionTimeTablePK().getDayId() + " " + DateFormatter.formatTime(table.getSectionTimeTablePK().getStartTime()) + " " + table.getLengthInMinutes());
+//                    Label label = new Label(hrs + ":00");
+//                    gdpTimeTable.add(label, 0, row++);
+//                    row++;
 //                }
-//            }
+
+                Label label = new Label(table.getSectionTimeTablePK().getDepartmentId() + " " + table.getSectionTimeTablePK().getCourseNumber());
+                gdpTimeTable.add(label, cols, rows++);
+                System.out.println("" + table.getSectionTimeTablePK().getDayId() + " " + DateFormatter.formatTime(table.getSectionTimeTablePK().getStartTime()) + " " + table.getLengthInMinutes());
+            }
+        }
 
 
     }
