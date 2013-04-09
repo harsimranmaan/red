a052e8a40294e0c3c6adf7150f2f491ff5ad9743
Harsimran Singh Maan <maan.harry@gmail.com>
2013-04-08 23:00:03 -0700

    Email Sender template

---
+++ Red/src/adg/red/controllers/admin/UserController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adg.red.controllers.admin;
+
+import adg.red.models.User;
+import adg.red.utils.Context;
+import adg.red.utils.EmailSender;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ * <p/>
+ * @author harsimran.maan
+ */
+public class UserController implements Initializable
+{
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        User currentUser = Context.getInstance().getCurrentUser();
+        EmailSender emailSender = new EmailSender(currentUser.getEmail(), "Did you really forget the password? LOL", currentUser.toString());
+        emailSender.send();
+
+    }
+}
