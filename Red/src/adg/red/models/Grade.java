661de7d50ccf7752e99d38c84ba51e51a08538e2
Witty <wittchot@mss.icics.ubc.ca>
2013-04-09 00:58:31 -0700

    fix faculty section to display according to action and make upload score to upload grade as well

--- Red/src/adg/red/models/Grade.java
+++ Red/src/adg/red/models/Grade.java
@@ -4,7 +4,10 @@
  */
 package adg.red.models;
 
+import adg.red.utils.LocaleManager;
+import adg.red.utils.RedEntityManager;
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -23,11 +26,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "Grade")
 @XmlRootElement
-@NamedQueries({
+@NamedQueries(
+        {
     @NamedQuery(name = "Grade.findAll", query = "SELECT g FROM Grade g"),
     @NamedQuery(name = "Grade.findByGradeId", query = "SELECT g FROM Grade g WHERE g.gradeId = :gradeId"),
-    @NamedQuery(name = "Grade.findByName", query = "SELECT g FROM Grade g WHERE g.name = :name")})
-public class Grade implements Serializable {
+    @NamedQuery(name = "Grade.findByName", query = "SELECT g FROM Grade g WHERE g.name = :name")
+})
+public class Grade implements Serializable
+{
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,57 +45,81 @@ public class Grade implements Serializable {
     @Column(name = "name")
     private String name;
 
-    public Grade() {
+    public Grade()
+    {
     }
 
-    public Grade(Integer gradeId) {
+    public Grade(Integer gradeId)
+    {
         this.gradeId = gradeId;
     }
 
-    public Grade(Integer gradeId, String name) {
+    public Grade(Integer gradeId, String name)
+    {
         this.gradeId = gradeId;
         this.name = name;
     }
 
-    public Integer getGradeId() {
+    public Integer getGradeId()
+    {
         return gradeId;
     }
 
-    public void setGradeId(Integer gradeId) {
+    public void setGradeId(Integer gradeId)
+    {
         this.gradeId = gradeId;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         int hash = 0;
         hash += (gradeId != null ? gradeId.hashCode() : 0);
         return hash;
     }
 
     @Override
-    public boolean equals(Object object) {
+    public boolean equals(Object object)
+    {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Grade)) {
+        if (!(object instanceof Grade))
+        {
             return false;
         }
         Grade other = (Grade) object;
-        if ((this.gradeId == null && other.gradeId != null) || (this.gradeId != null && !this.gradeId.equals(other.gradeId))) {
+        if ((this.gradeId == null && other.gradeId != null) || (this.gradeId != null && !this.gradeId.equals(other.gradeId)))
+        {
             return false;
         }
         return true;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "adg.red.models.Grade[ gradeId=" + gradeId + " ]";
     }
-    
+
+    public static Grade getByName(String name) throws Exception
+    {
+        List<Grade> gradeList = RedEntityManager.getEntityManager().createNamedQuery("Grade.findByName").setParameter("name", name).getResultList();
+        if (gradeList.size() == 1)
+        {
+            return gradeList.get(0);
+        }
+        else
+        {
+            throw new Exception(LocaleManager.get(100));
+        }
+    }
 }
