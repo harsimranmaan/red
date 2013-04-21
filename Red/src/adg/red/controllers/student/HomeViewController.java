41cd2bcdafa4e3a2643c4732fade0129d057c774
nicole <nicoleyu2003@gmail.com>
2013-04-20 23:38:12 -0700

    code modification on home screen

--- Red/src/adg/red/controllers/student/HomeViewController.java
+++ Red/src/adg/red/controllers/student/HomeViewController.java
@@ -42,14 +42,11 @@ public class HomeViewController implements Initializable
         BreadCrumbController.renderBreadCrumb("student/HomeView");
         currentUser = Context.getInstance().getCurrentUser();
         nameLabel.setText("Hi " + currentUser.getFirstName() + ", welcome to the RED enrolment system.");
-        nameLabel.setFont(Font.font ("Arial", 20));
-        //nameLabel.setTextFill(Color.web("#FF0000"));
         int nUnread = MessageReceiver.getUnreadMessageByReceiverId(currentUser.getUsername());
         if (nUnread <= 1)
             msgLabel.setText("You have " + nUnread + " unread message.");
         else
             msgLabel.setText("You have " + nUnread + " unread messages.");
-        msgLabel.setFont(Font.font ("Arial", 20));
-        //msgLabel.setTextFill(Color.web("#000000"));
+       
     }
 }
